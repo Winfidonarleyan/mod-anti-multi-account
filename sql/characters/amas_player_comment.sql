6bdaa57b0b86ddbc989a0f0687f4d333efdb5db6
Kargatum <dowlandtop@yandex.ru>
2018-09-03 02:58:52 +0700

    Added missing table for comment

---
+++ sql/characters/amas_player_comment.sql
@@ -0,0 +1,14 @@
+-- ----------------------------
+-- Table structure for amas_player_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `amas_player_comment`;
+CREATE TABLE `amas_player_comment`  (
+  `CommentID` int(11) NOT NULL AUTO_INCREMENT,
+  `PlayerGuid` int(20) NOT NULL,
+  `PlayerAccount` int(20) NOT NULL,
+  `Comment` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `Date` timestamp NULL DEFAULT '0000-00-00 00:00:00',
+  `CommenterGuid` int(20) NOT NULL DEFAULT 0,
+  `CommenterAccount` int(20) NOT NULL,
+  PRIMARY KEY (`CommentID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
