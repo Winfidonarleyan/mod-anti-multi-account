4d0cd2a7fc9101cdd3106b607d26ecd21a0e491f
Kargatum <dowlandtop@yandex.ru>
2018-10-01 21:04:34 +0700

    DB/Strings: Add full color variables according to the last commit
    * Need retest full locales

--- sql/world/trinity_string.sql
+++ sql/world/trinity_string.sql
@@ -1,56 +1,61 @@
-SET @FirstID = 40037;
-SET @r = "|r"; -- Reset color
-SET @red = "|cFFFF0000";
+SET 
+@FirstID = 40037,
+@r = "|r", -- Reset color
+@red = "|cFFFF0000",
+@green = "|cFF00FF00",
+@n = "\n", -- New line
+@logo = "|cFFFF0000#|r |cFF00FF00", -- Winfi logo
+@sytemlogo = "[|cFFFF0000AMAS|r]:|cFF00FF00 "; -- AMAS logo
 
 DELETE FROM `trinity_string` WHERE `entry` BETWEEN @FirstID AND @FirstID + 31;
 INSERT INTO `trinity_string`(`entry`, `content_default`, `content_loc8`) VALUES
 
 -- Amas general
-(@FirstID, CONCAT( '[', @red,'AMAS', @r, ']: System disabled'), CONCAT( '[', @red,'AMAS', @r, ']: Система выключена'),
+(@FirstID, CONCAT(@sytemlogo, 'System disabled'), CONCAT(@sytemlogo, 'Система выключена')),
 
 -- Announce GM
-(@FirstID + 1, '[|cFFFF0000AMAS|r]: |cff00ff00Character|r %s |cff00ff00just logged in and has|r %.2f |cff00ff00AMAS points.|r', '[|cFFFF0000AMAS|r]: |cff00ff00Игрок|r %s |cff00ff00имеет|r %.2f |cff00ff00очков подозрительности|r'),
+(@FirstID + 1, CONCAT(@sytemlogo, @green, 'Character', @r, ' %s ', @green, 'just logged in and has', @r, ' %.2f ', @green, 'AMAS points.', @r), CONCAT(@sytemlogo, @green, 'Игрок', @r, ' %s ', @green , 'имеет', @r, ' %.2f ', @green, 'очков подозрительности', @r)),
 
 -- AMAS info
-(@FirstID + 2, 'Character %s\n-------\nTotal points: |cffff0000%.2f|r\n|cffff0000%.2f|r - Time played on account: %s\n|cffff0000%.2f|r - Average item level: %u\n|cffff0000%.2f|r - Unused talent points: %u\n|cffff0000%.2f|r - Completed quests count: %u\n|cffff0000%.2f|r - Friend count: %u\n|cffff0000%.2f|r - Money: %s\n|cffff0000%.2f|r - Honor: %u. Kills: %u\n|cffff0000%.2f|r - IP: %s. Unique? (%s).%s%s\n|cffff0000%.2f|r - Missing trainer spells: %u\n|cffff0000%.2f|r - Current zone: %u - %s%s\n|cffff0000%.2f|r - Profession count: %u\n|cffff0000%.2f|r - Average session time: %s.\nTime played on character: %s\nComment count: %i', 'Игрок %s. |cffff0000%.2f|r\n-------\n|cffff0000%.2f|r - Игровое время на аккаунте: %s\n|cffff0000%.2f|r - Средний уровень предметов: %u\n|cffff0000%.2f|r - Свободных талантов: %u\n|cffff0000%.2f|r - Пройденных заданий: %u\n|cffff0000%.2f|r - Количество друзей: %u\n|cffff0000%.2f|r - Деньги: %s\n|cffff0000%.2f|r - Очки чести: %u. Убийства: %u\n|cffff0000%.2f|r - Айпи: %s. Унивальное? (%s).%s%s\n|cffff0000%.2f|r - Не изученных заклинаний: %u\n|cffff0000%.2f|r - Находится в зоне: %u - %s%s\n|cffff0000%.2f|r - Количество профессий: %u\n|cffff0000%.2f|r - Среднее время сессии: %s\nИгровое время на персонаже: %s\nКоличество комментариев: %i'),
+(@FirstID + 2, CONCAT('Character %s', @n, '-------', @n, 'Total points: ', @red, '%.2f', @r, @n, @red, '%.2f', @r, ' - Time played on account: %s', @n, @red, '%.2f', @r, ' - Average item level: %u', @n, @red, '%.2f', @r, ' - Unused talent points: %u', @n, @red, '%.2f', @r, ' - Completed quests count: %u', @n, @red, '%.2f', @r, ' - Friend count: %u', @n, @red, '%.2f', @r, ' - Money: %s', @n, @red, '%.2f', @r, ' - Honor: %u. Kills: %u', @n, @red, '%.2f', @r, ' - IP: %s. Unique? (%s).%s%s', @n, @red, '%.2f', @r, ' - Missing trainer spells: %u', @n, @red, '%.2f', @r, ' - Current zone: %u - %s%s', @n, @red, '%.2f', @r, ' - Profession count: %u', @n, @red, '%.2f', @r, ' - Average session time: %s.', @n, 'Time played on character: %s', @n, 'Comment count: %i'), CONCAT('Игрок %s. |cffff0000%.2f|r', @n, '-------', @n, @red, '%.2f', @r, ' - Игровое время на аккаунте: %s', @n, @red, '%.2f', @r, ' - Средний уровень предметов: %u', @n, @red, '%.2f', @r, ' - Свободных талантов: %u', @n, @red, '%.2f', @r, ' - Пройденных заданий: %u', @n, @red, '%.2f', @r, ' - Количество друзей: %u', @n, @red, '%.2f', @r, ' - Деньги: %s', @n, @red, '%.2f', @r, ' - Очки чести: %u. Убийства: %u', @n, @red, '%.2f', @r, ' - Айпи: %s. Унивальное? (%s).%s%s', @n, @red, '%.2f', @r, ' - Не изученных заклинаний: %u', @n, @red, '%.2f', @r, ' - Находится в зоне: %u - %s%s', @n, @red, '%.2f', @r, ' - Количество профессий: %u', @n, @red, '%.2f', @r, ' - Среднее время сессии: %s', @n, 'Игровое время на персонаже: %s', @n, 'Количество комментариев: %i')),
 (@FirstID + 3, 'Character %s is offline and has no entry in the Database.', 'Игрок %s не в сети и ещё не сохранён в БД'),
 
 -- AMAS warning zone
-(@FirstID + 4, 'Warning zone list:', 'Список подозрительных зон:'),
-(@FirstID + 5, '|cff00ff00You added a new warning zone:|r %u - %s', '|cff00ff00Вы добавили подозрительную зону:|r %u - %s'),
-(@FirstID + 6, '|cff00ff00Not found in warning zone:|r %u - %s', '|cff00ff00Не найдено среди подозрительных зон:|r %u - %s'),
-(@FirstID + 7, '|cff00ff00You have deleted warning zone:|r %u - %s', '|cff00ff00Вы удалили подозрительную зону:|r %u - %s'),
-(@FirstID + 8, '|cff00ff00This warning zone already exists:|r %u - %s', '|cff00ff00Такая подозрительная зона уже существует:|r %u - %s'),
-(@FirstID + 9, '|cff00ff00List warning zone is empty.', '|cff00ff00Список подозрительных зон пуст'),
+(@FirstID + 4, CONCAT(@green, 'Warning zone list:'),CONCAT(@green, 'Список подозрительных зон:')),
+(@FirstID + 5, CONCAT(@green, 'You added a new warning zone:', @r, ' %u - %s'), CONCAT(@green, 'Вы добавили подозрительную зону', @r, ' %u - %s')),
+(@FirstID + 6, CONCAT(@green, 'Not found in warning zone', @r, ' %u - %s'), CONCAT(@green, 'Не найдено среди подозрительных зон', @r, ' %u - %s')),
+(@FirstID + 7, CONCAT(@green, 'You have deleted warning zone', @r, ' %u - %s'), CONCAT(@green, 'Вы удалили подозрительную зону', @r, ' %u - %s')),
+(@FirstID + 8, CONCAT(@green, 'This warning zone already exists', @r, ' %u - %s'), CONCAT(@green, 'Такая подозрительная зона уже существует', 	@r, ' %u - %s')),
+(@FirstID + 9, CONCAT(@green, 'List warning zone is empty.'), CONCAT(@green, 'Список подозрительных зон пуст')),
 (@FirstID + 10, ' (is warning zone).', ' (подозрительная зона)'),
-(@FirstID + 11, '|cff00ff00Not found zoneid:|r %u', '|cff00ff00Такой зоны не существует:|r %u'),
+(@FirstID + 11, CONCAT(@green, 'Not found zoneid:', @r, ' %u'), CONCAT(@green, 'Такой зоны не существует:', @r, ' %u')),
 
 -- AMAS list
-(@FirstID + 12, '|cFFFF0000#|r |cff00ff00Suspicious online players:|r', '|cFFFF0000#|r |cff00ff00Список топа онлайн игроков по очкам подозреваемости:|r'),
-(@FirstID + 13, '|cFFFF0000#|r |cff00ff00There is no suspicious online character.|r', '|cFFFF0000#|r |cff00ff00Такие онлайн игроки не найдены.|r'),
-(@FirstID + 14, '|cFFFF0000#|r |cff00ff00Suspicious offline players:|r', '|cFFFF0000#|r |cff00ff00Список топа оффлайн игроков по очкам подозреваемости:|r'),
-(@FirstID + 15, '|cFFFF0000#|r |cff00ff00There is no suspicious offline character.|r', '|cFFFF0000#|r |cff00ff00Такие оффлайн игроки не найдены.|r'),
+(@FirstID + 12, CONCAT(@logo, 'Suspicious online players:', @r), CONCAT(@logo, 'Список топа онлайн игроков по очкам подозреваемости', @r)),
+(@FirstID + 13, CONCAT(@logo, 'There is no suspicious online character.', @r), CONCAT(@logo, 'Такие онлайн игроки не найдены.', @r)),
+(@FirstID + 14, CONCAT(@logo, 'Suspicious offline players:', @r), CONCAT(@logo, 'Список топа оффлайн игроков по очкам подозреваемости', @r)),
+(@FirstID + 15, CONCAT(@logo, 'There is no suspicious offline character.', @r), CONCAT(@logo, 'Такие оффлайн игроки не найдены.', @r)),
 
 -- AMAS comment
-(@FirstID + 16, '|cFFFF0000#|r |cff00ff00Enter character\'s name and type your comment.', '|cFFFF0000#|r |cff00ff00Введите имя персонажа и комментарий'),
-(@FirstID + 17, '|cFFFF0000#|r |cff00ff00Type your comment after the character\'s name.', '|cFFFF0000#|r |cff00ff00Введите комментарий'),
-(@FirstID + 18, '|cFFFF0000#|r |cff00ff00Comment ID required.', '|cFFFF0000#|r |cff00ff00Укажите номер комментария'),
-(@FirstID + 19, '|cFFFF0000#|r |cff00ff00Type the comment ID and then your comment.', '|cFFFF0000#|r |cff00ff00Укажите номер комментария и сообщение'),
-(@FirstID + 20, '|cFFFF0000#|r |cff00ff00Comment ID not found:|r %u.', '|cFFFF0000#|r |cff00ff00Не найден комментарий под номером:|r %u'),
-(@FirstID + 21, '|cFFFF0000#|r |cff00ff00No comment found for character:|r %s.', '|cFFFF0000#|r |cff00ff00Не найдены комментарии для игрока:|r %s'),
-(@FirstID + 22, '|cFFFF0000#|r |cff00ff00Entered invalid comment ID.', '|cFFFF0000#|r |cff00ff00Неправильно введён номер комментария'),
-(@FirstID + 23, '|cFFFF0000#|r |cff00ff00Comment added. \n|cFFFF0000#|r |cff00ff00Character:|r %s |cff00ff00 \n|cFFFF0000#|r |cff00ff00Comment:|r |cFFFF0000%s|r.', '|cFFFF0000#|r |cff00ff00Вы добавили комментарий игроку|r %s |cff00ff00-|r |cFFFF0000%s|r'),
-(@FirstID + 24, '|cFFFF0000#|r |cff00ff00Comment deleted. \n|cFFFF0000# |cff00ff00CommentID:|r %u. \n|cFFFF0000# |cff00ff00Comment:|r |cFFFF0000%s|r.', '|cFFFF0000#|r |cff00ff00Удалён комментарий. Номер:|r %u. |cff00ff00Сообщение|r |cFFFF0000%s|r'),
-(@FirstID + 25, '|cFFFF0000#|r |cff00ff00Edited comment. CommentID:|r %u. |cff00ff00CommentTexts:|r |cFFFF0000%s|r -> |cFFFF0000%s|r.', '|cFFFF0000#|r |cff00ff00Замена комментария. Номер:|r %u. |cff00ff00Сообщения:|r |cFFFF0000%s|r -> |cFFFF0000%s|r'),
-(@FirstID + 26, '|cFFFF0000#|r |cff00ff00List comments for character|r %s.', '|cFFFF0000#|r |cff00ff00Список комментариев для игрока|r %s'),
-(@FirstID + 27, '|cFFFF0000#|r |cff00ff00CommentID:|r %u | |cff00ff00Commenter:|r %s | |cff00ff00Date:|r %s | |cff00ff00Comment:|r %s', '|cFFFF0000#|r |cff00ff00Номер:|r %u. |cff00ff00Автор:|r %s. |cff00ff00Дата:|r %s. |cff00ff00Комментарий:|r %s.'),
+(@FirstID + 16, CONCAT(@logo, 'Enter character\'s name and type your comment.'), CONCAT(@logo, 'Введите имя персонажа и комментарий')),
+(@FirstID + 17, CONCAT(@logo, 'Type your comment after the character\'s name.'), CONCAT(@logo, 'Введите комментарий')),
+(@FirstID + 18, CONCAT(@logo, 'Comment ID required.'), CONCAT(@logo, 'Укажите номер комментария')),
+(@FirstID + 19, CONCAT(@logo, 'Type the comment ID and then your comment.'), CONCAT(@logo, 'Укажите номер комментария и сообщение')),
+(@FirstID + 20, CONCAT(@logo, 'Comment ID not found:', @r, ' %u.'), CONCAT(@logo, 'Не найден комментарий под номером:', @r, ' %u.')),
+(@FirstID + 21, CONCAT(@logo, 'No comment found for character:', @r, ' %s.'), CONCAT(@logo, 'Не найдены комментарии для игрока:', @r, ' %s.')),
+(@FirstID + 22, CONCAT(@logo, 'Entered invalid comment ID.'), CONCAT(@logo, 'Неправильно введён номер комментария')),
+(@FirstID + 23, CONCAT(@logo, 'Comment added.', @n, @logo, 'Character:', @r, ' %s', @n, @logo, 'Comment:', @r, @red, ' %s', @r), CONCAT(@logo, 'Вы добавили комментарий игроку', @r, ' %s ', @green, '-', @r, @red, '%s', @r)),
+(@FirstID + 24, CONCAT(@logo, 'Comment deleted.', @n, @logo, 'CommentID:', @r, ' %u.', @n, @logo, 'Comment:', @r, @red, '%s', @r, '.'), CONCAT(@logo, 'Удалён комментарий. Номер:', @r, ' %u. ', @green, 'Сообщение:', @r, @red, '%s', @r)),
+(@FirstID + 25, CONCAT(@logo, 'Edited comment. CommentID:', @r, ' %u. ', @green, 'CommentTexts: ', @r, @red, '%s', @r, ' -> ', @red, '%s', @r), CONCAT(@logo, 'Замена комментария. Номер:', @r, ' %u. ', @green, 'Сообщение:', @r, @red, '%s', @r)),
+(@FirstID + 26, CONCAT(@logo, 'List comments for character', @r, ' %s.'), CONCAT(@logo, 'Список комментариев для игрока', @r, '%s')),
+(@FirstID + 27, CONCAT(@logo, 'CommentID:', @r, ' %u | ', @green, 'Commenter:', @r, ' %s | ', @green, 'Date:', @r, ' %s |', @green, 'Comment: ', @r, ' %s'), CONCAT(@logo, 'Номер:', @r, ' %u. ', @green, 'Автор:', @r, ' %s. ', @green, 'Дата:', @r, ' %s. ', @green, 'Комментарий:', @r, '%s.')),
 
 -- Amas same ip
 (@FirstID + 28, ' PERFECT MATCH with account ', ' Совпадение с акками '),
 (@FirstID + 29, ' Same first byte ip ', ' Cовпадение первых цифр айпи'),
 
 -- Amas confirmed
-(@FirstID + 30, '[|cFFFF0000AMAS|r]: |cff00ff00Confirmed player|r %s |cff00ff00has|r %.2f |cff00ff00warning point. This more min|r %u', '[|cFFFF0000AMAS|r]: |cff00ff00Подтверждённый игрок|r %s |cff00ff00имеет|r %.2f |cff00ff00очков подозрительности. Это больше минимума|r %u'),
+(@FirstID + 30, CONCAT(@sytemlogo, @green, 'Confirmed player', @r, ' %s ', @green, 'has', @r, ' %.2f ', @green, 'warning point. This more min', @r, ' %u'), CONCAT(@sytemlogo, @green, 'Подтверждённый игрок', @r, ' %s ', @green, 'имеет', @r, ' %.2f ', @green, 'очков подозрительности. Это больше минимума', @r, ' %u')),
 
 -- Amas mail
-(@FirstID + 31, 'Recipient %s\n- Money: %u\n- Item: %u\n- Only Text: %u\n- Auction House: %u\n- Account: %s - %u', 'Получатель %s\n- Деньги: %u\n- Итемы: %u\n- Только текст: %u\n- Аукционный дом: %u\n- Аккаунт: %s - %u');
+(@FirstID + 31, CONCAT('Recipient %s', @n, '- Money: %u', @n, '- Item: %u', @n, '- Only Text: %u', @n, '- Auction House: %u', @n, '- Account: %s - %u'), CONCAT('Получатель %s', @n, '- Деньги: %u', @n, '- Итемы: %u', @n, '- Только текст: %u', @n, '- Аукционный дом: %u', @n, '- Аккаунт: %s - %u'));
