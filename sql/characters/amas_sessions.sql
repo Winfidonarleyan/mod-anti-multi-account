53653cf783cdb457ff71fb2ab891df4b661d3f7a
Kargatum <dowlandtop@yandex.ru>
2018-10-04 02:09:03 +0700

    Fix typo in `amas_session`. Rename PlayerGUID to CharacterGUID
    Close https://github.com/Winfidonarleyan/mod-anti-multi-account/issues/36

--- sql/characters/amas_sessions.sql
+++ sql/characters/amas_sessions.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS `amas_session`;
-CREATE TABLE `amas_session` (
-  `PlayerGUID` int(11) NOT NULL DEFAULT '0',
+DROP TABLE IF EXISTS `amas_sessions`;
+CREATE TABLE `amas_sessions` (
+  `CharacterGUID` int(11) NOT NULL DEFAULT '0',
   `SessionCount` int(10) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`PlayerGUID`)
+  PRIMARY KEY (`CharacterGUID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
